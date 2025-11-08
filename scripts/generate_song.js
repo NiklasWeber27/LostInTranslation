dfb6a700b2f8c09950694b2d6988c4a39712b0c6
NiklasWeber27 <niklasweber910@gmail.com>
2025-11-08 18:30:22 +0100

    changes

--- scripts/generate_song.js
+++ scripts/generate_song.js
@@ -6,8 +6,7 @@ const CACHE_FILE = "data/cache_translations.json";
 const OUTPUT_FILE = "data/song_of_the_day.json";
 
 async function translateText(text, lang = "de") {
-   const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|${lang}`;
-
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|${lang}`;
   try {
     const res = await fetch(url);
     const data = await res.json();
